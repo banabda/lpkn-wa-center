51c94e97efa0c264a06b4740d8944fbefb8aefd4
akbar <nandasholatula@gmail.com>
2021-09-09 15:38:04 +0700

    button pink

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                 class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white">
                 <div class="sm:w-1/2 xl:w-3/5 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden bg-purple-900 text-white bg-no-repeat bg-cover relative"
                     style="background-image: url(https://a-cloud.b-cdn.net/media/iW=5000&iH=any/de4d3251e11e024ad5c12a002a43fbea.jpg);">
-                    <div class="absolute bg-gradient-to-b bg-pink-002 to-blue-500 opacity-75 inset-0 z-0"></div>
+                    <div class="absolute bg-gradient-to-b bg-pink-700 to-blue-500 opacity-75 inset-0 z-0"></div>
                     <div class="w-full  max-w-md z-10">
                         <div class="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6">Arayu
                             Whatsapp
@@ -128,7 +128,7 @@
                             </div>
                             <div>
                                 <button type="submit"
-                                    class="w-full flex justify-center bg-gradient-to-r bg-pink-001 bg-pink-001  hover:from-pink-500 hover:from-pink-500 hover:from-pink-500 text-gray-100 p-4  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
+                                    class="w-full flex justify-center bg-gradient-to-r bg-pink-700 bg-pink-500  hover:from-pink-500 hover:from-pink-500 hover:from-pink-500 text-gray-100 p-4  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
                                     {{ __('Masuk') }}
                                 </button>
                             </div>
